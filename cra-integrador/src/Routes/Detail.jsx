0871ab59b55704b864a4ea96f4619e8c6fbac285
Renzo Borraccini <106200790+RenzoBor@users.noreply.github.com>
2024-02-22 09:53:42 -0300

    "checkpoint"

--- cra-integrador/src/Routes/Detail.jsx
+++ cra-integrador/src/Routes/Detail.jsx
@@ -1,12 +1,22 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import imgCarpa from '../img/carpa-playera.jpg'
 
 function Detail(props){
-    const [product,setProduct] = useState([])
+    const [product,setProduct] = useState({})
+    const [id,setId] = useState()
+    useEffect(() => {
+      const currentUrl = window.location.pathname;
+      const ultimoCaracter = currentUrl.slice(-1);
+      setId(ultimoCaracter)
+    },[]);
 
     useEffect(() => {
-        const productId = props.match.params.id;
-        fetch( /*peticion a la api con el product id */)
+        const url = `/Productos/${id}`
+        const settings = {
+            method:"GET"
+        }
+        fetch(url,settings)
         .then((response) => response.json())
         .then((data) => {
           setProduct(data);
@@ -14,19 +24,21 @@ function Detail(props){
         .catch((error) => {
           console.error('Error al obtener detalles del producto:', error);
         });
-    }, [props.match.params.id]);
-  /* useEffect(() => {
+    }, []);
+
+   /*useEffect(() => {
     setProduct({
         id : 1,
-        nombre : "carpa 1",})
-   },[]) */
+        nombre : "carpa 1",
+        img: imgCarpa});
+   },[])*/
   
 
     return(
         <section className="detail">
-            <header><h2>{product.nombre}</h2>   <button onClick={useNavigate(-1)}>{"<--"}</button> </header>
+            <div id="detail-header"><h2 className="detail-header-item">{product.nombre}</h2>   <button className="detail-header-item" onClick={useNavigate(-1)}>{"<--"}</button> </div>
             <main>
-                <img src={product.img[0]} alt="Imagen de producto" />
+                <img src={product.img} alt="Imagen de producto" />
                 <p>{product.descripcion}</p>
             </main>
         </section>
