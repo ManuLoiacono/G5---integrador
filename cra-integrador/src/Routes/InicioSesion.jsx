9f5264703463823c930b2f4e834ef99c9a5d7bed
Renzobor <renzo-ejem@hotmail.com>
2024-03-01 20:17:49 -0300

    Login y registro ruteado. Login estilizado.

--- cra-integrador/src/Routes/InicioSesion.jsx
+++ cra-integrador/src/Routes/InicioSesion.jsx
@@ -30,12 +30,16 @@ function InicioSesion(){
 
     return(
         <div className="input-user-card-container">
-            <section className="imput-user-card">
+            <section className="input-user-card">
                 <h2>Iniciar sesión</h2>
-                <input type="text" value={email} onChange={(e) => setEmail(e.target.value)}/>
-                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-                <button>Ingresar</button>
-                <p>Necesitas una cuenta?</p> <Link to={'/registrar-usuario'}><p className="registrarse">Registrarse</p></Link>
+                <form action="POST">
+                <input id="get-mail" className="login-input" type="text" value={email} placeholder="Tu Email" onChange={(e) => setEmail(e.target.value)}/>
+                <input id ="get-password"className="login-input" type="password" value={password} placeholder="Tu Contraseña" onChange={(e) => setPassword(e.target.value)}/>
+                <button className="login-button">Ingresar</button>
+                </form>
+                <div className="login-text-container">
+                <p className="login-text">¿Necesitas una cuenta?</p> <Link to={'/registrar-usuario'}><p className="registrarse">Registrarse</p></Link>
+                </div>
 
             </section>
         </div>
