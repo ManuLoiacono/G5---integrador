4cb523df4914d1777e09f361013c124667c829fb
Andres Aristimuño <andaristis@gmail.com>
2024-03-21 02:00:26 -0300

    Ajustes de estilo en Detail

--- cra-integrador/src/Routes/Detail.jsx
+++ cra-integrador/src/Routes/Detail.jsx
@@ -34,6 +34,12 @@ function Detail() {
     fetchData();
   }, [params.id]);
   console.log(product.imagenes);
+    
+  const [mostrarContenido, setMostrarContenido] = useState(false);
+  
+  const toggleContenido = () => {
+    setMostrarContenido(!mostrarContenido);
+  };
   return (
     <section className="detail">
       <div id="detail-header">
@@ -76,6 +82,37 @@ function Detail() {
               <Link to={`/productos/${params.id}/galeria`}><p>Ver más...</p></Link>
               <Outlet />
             </div>
+            <div id="caracteristicas">
+      <p className="titulo" onClick={toggleContenido}>Políticas y devoluciones</p>
+      {mostrarContenido && (
+        <div className="contenido">
+          <ul>
+          <p>Políticas generales:</p>
+            <li>El cliente debe leer y aceptar todas las políticas y condiciones antes de realizar una reserva o alquiler en Terrarent.</li>
+            <li>Las tarifas de alquiler están sujetas a cambios sin previo aviso.</li>
+            <li>Los precios pueden variar según la temporada y la disponibilidad del producto.</li>
+          </ul>
+          <ul>
+          <p>Políticas de Devolución del Producto:</p>
+            <li>El cliente es responsable de devolver los productos de alquiler en la fecha y hora acordadas durante la reserva.</li>
+            <li>Los productos deben ser devueltos en las mismas condiciones en las que fueron entregados. Se aplicarán cargos adicionales por daños o pérdida.</li>
+            <li>Se proporcionará un período de gracia de 30 minutos para la devolución de los productos. Después de este tiempo, se aplicarán cargos adicionales.</li>
+          </ul>
+          <ul>
+          <p>Políticas de Cancelación:</p>
+            <li>Las cancelaciones deben realizarse con al menos 48 horas de anticipación para recibir un reembolso completo.</li>
+            <li>Se aplicará un cargo por cancelación del 50% para cancelaciones realizadas dentro de las 48 horas previas a la fecha de inicio del alquiler.</li>
+            <li>Las cancelaciones realizadas dentro de las 24 horas previas al inicio del alquiler no serán elegibles para reembolso.</li>
+          </ul>
+          <ul>
+          <p>Políticas de Reserva:</p>
+            <li>Las reservas deben realizarse con al menos 24 horas de anticipación para garantizar la disponibilidad del producto.</li>
+            <li>Las reservas están sujetas a disponibilidad y pueden requerir un depósito anticipado para confirmar la reserva.</li>
+            <li>Terrarent se reserva el derecho de cancelar o modificar una reserva en caso de circunstancias imprevistas o situaciones fuera de nuestro control.</li>
+          </ul>
+        </div>
+      )}
+    </div>
         </div>
         <div id="under-img">
           <div className="descripcion-container">
